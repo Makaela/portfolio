c5a6d3d02a16b4bd4ff670f6f04788ae4fe5a6fc
Makaela <MartinezM2019@365.bcsd.org>
2017-07-18 15:25:20 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+alert ("Hey! Welcome to my Page!")
+
+function bigImg(x) {
+    x.style.height = "300px";
+    x.style.width = "400px";
+}
+
+function normalImg(x) {
+    x.style.height = "150px";
+    x.style.width = "200px";
+}
